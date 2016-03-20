31cf56b191eae842d78209f253b75caf06a2b95d
maratkanov-a <a.s.maratkanov@gmail.com>
2016-03-21 00:01:30 +0300

    finished with common

--- views/common.py
+++ views/common.py
@@ -1,19 +1,46 @@
 from flask import Blueprint
-from response_json import response
+from response_json import *
 
 common = Blueprint('common', __name__)
 
 
-# @common.route("/clear", methods=['POST'])
-@common.route("/clear")
+@common.route("/clear", methods=['POST'])
 def delete_all():
-    # TODO
+
+    delete_list = ['User', 'Forum', 'Thread', 'Post', 'Follow', 'Subscribe']
+
+    c, conn = connection()
+
+    for el in delete_list:
+        try:
+            c.execute(''' delete '{}' from '{}' '''.format(el, el))
+        except (MySQLdb.Error, MySQLdb.Warning):
+            conn.close()
+            return response(4, 'Unknown error')
+
+    conn.close()
     return response(0, 'OK')
 
 
-# @common.route("/clear", methods=['GET'])
-@common.route("/status")
+@common.route("/status/", methods=['GET'])
 def get_status():
-    # TODO
-    result = ' lalaala'
-    return response(0, result)
+
+    c, conn = connection()
+
+    count_list = ['User', 'Forum', 'Thread', 'Post']
+
+    count_dict = {}
+
+    for el in count_list:
+        try:
+            c.execute(''' select count(*) from {} '''.format(el))
+            count_dict[el] = c.fetchall()[0][0]
+        except (MySQLdb.Error, MySQLdb.Warning):
+            conn.close()
+            return response(4, 'Unknown error')
+
+    res = count_dict
+
+    conn.close()
+
+    return response(0, res)
